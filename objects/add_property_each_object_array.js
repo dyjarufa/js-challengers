64d6b8cc671c4059ae672f983dff90ca407e1e36
Jady Rufino <jadyrufa@gmail.com>
2021-06-03 09:14:29 -0300

    add new object challenger

---
+++ objects/add_property_each_object_array.js
@@ -0,0 +1,20 @@
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tipp: try not to mutate the original array
+
+// function myFunction(arr, str) {
+//   return arr.map((element) => {
+//     element.continent = str;
+//     return element;
+//   })
+// }
+
+//Author's solution
+
+function myFunction(arr, str) {
+  return arr.map((obj) => ({ ...obj, continent: str }));
+}
+
+
+console.log(myFunction([{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'))
